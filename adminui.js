89bee32a14248d10195fe365a34e39957a4913a9
jstanley14 <jstanley@middil.com>
2017-12-18 13:30:09 -0500

    CAP-2439 - sorta works

--- adminui.js
+++ adminui.js
@@ -1,155 +1,165 @@
-let fontColorChoices = ['blue', 'black'];
-let fontWeightChoices = ['normal', 'lighter', 'heavier'];
-let fontFamilyChoices = ['Arial', 'Helvetica'];
-let textTransformChoices = ['lowercase', 'uppercase', 'capitalize', 'none'];
 
+let adminUI = {
+    subject: config.subjects[0],
+    gradeLevel: config.gradeLevels[0],
+    theme: {},
 
-let config = {
-    generic: {
-        fontFamily: fontFamilyChoices
+    initUI: function() {
+        $('#grade_level, #subject, #elementPane').empty();
+        config.gradeLevels.forEach(function(grade) {
+            let option = $('<option/>');
+            option.text(grade);
+            option.val(grade);
+            $('#grade_level').append(option);
+        });
+        config.subjects.forEach(function(subject) {
+            let option = $('<option/>');
+            option.text(subject);
+            option.val(subject);
+            $('#subject').append(option);
+        });
+
+        $('#grade_level').change(function() {
+            adminUI.gradeLevel = $('#grade_level').find('option:selected').val();
+            $('#elementPane').empty();
+            adminUI.theme = {};
+            adminUI.loadSelections();
+        });
+
+        $('#subject').change(function() {
+            adminUI.subject = $('#subject').find('option:selected').val()
+        });
+
+        adminUI.loadSelections();
     },
-    h2: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [16, 18, 20, 22],
-        fontFamily: fontFamilyChoices,
-        textTransform: textTransformChoices
+
+    loadSelections: function() {
+        let elms = Object.filter(config.elements, elm =>
+            elm.gradeLevel === 'all' || elm.gradeLevel === adminUI.gradeLevel
+        );
+
+        // Group elms by category.
+        let elmsByCategory = {};
+        Object.keys(elms).forEach(key => {
+            (elmsByCategory[elms[key].category] = elmsByCategory[elms[key].category] || []).push(elms[key]);
+        });
+
+        // Create categories.
+        Object.keys(elmsByCategory).forEach(key => {
+            let cat = $('<div/>');
+            cat.append($('<h1/>').text(key));
+
+            let elmsBySection = {};
+            elmsByCategory[key].forEach(elm => {
+                (elmsBySection[elm.section] = elmsBySection[elm.section] || []).push(elm)
+            });
+
+            console.log(elmsBySection['Section 1']);
+
+            // Create sections.
+            Object.keys(elmsBySection).forEach(key => {
+                let sec = $('<div/>');
+                sec.append($('<h3/>').text(key));
+
+                elmsBySection[key].forEach(elm => {
+                   let e = $('<div/>');
+                   e.attr('id', elm.name);
+                   let elmName = $('<h5/>').addClass('elementChoice').text(elm.friendlyName);
+                   e.append(elmName);
+
+                   // Create element attribute selections.
+                   elm.options.forEach(opt => {
+                       let attr = $('<div/>');
+                       let optName = Object.keys(opt)[0];
+                       attr.attr('id', optName);
+                       attr.addClass('elementAttributeChoice');
+                       attr.append($('<span/>').text(optName));
+                       let select = $('<select/>');
+                       opt[optName].forEach(choice => {
+                           let option = $('<option/>');
+                           option.text(choice);
+                           option.val(choice);
+                           select.append(option);
+                       });
+                       attr.append(select);
+                       e.append(attr);
+                   });
+                   sec.append(e);
+                });
+
+                cat.append(sec);
+            });
+
+            $('#elementPane').append(cat);
+        });
+        $('.elementChoice').click(evt => {
+            $(evt.target).siblings().toggle()
+        } );
     },
-    h3: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [16, 18, 20, 22],
-        textTransform: textTransformChoices,
-        fontFamily: fontFamilyChoices
-    },
-    h4: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [16, 18, 20, 22],
-        textTransform: textTransformChoices,
-        fontFamily: fontFamilyChoices
-    },
-    h5: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [16, 18, 20, 22],
-        textTransform: textTransformChoices
-    },
-    h6: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [16, 18, 20, 22],
-        textTransform: textTransformChoices
-    },
-    p: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [16, 18, 20, 22],
-        fontFamily: fontFamilyChoices
-    },
-    hyperlink: {
-        fontColor: fontColorChoices,
-        fontWeight: fontWeightChoices,
-        fontSize: [10, 12, 14, 16],
-        fontFamily: fontFamilyChoices,
-        focus: [],
-        visited: [],
-        active: [],
-        hover: [],
-        decoration: ['none', 'underline']
-    },
-    verticalBar: {
-        color: ['red', 'green', 'blue', 'black', 'yellow']
+
+    generateVariablesFile: () => {
+        // For anything on the page, use current selection
+        // Otherwise, use 1st choice in array in config
+        // (Temporary logic - need to save choices and distinguish
+        //   choices by subject (when applicable.))
+
+        Object.keys(config.elements).forEach(key => {
+            let elm = config.elements[key];
+            // Need to find this way because ID can have spaces in it.
+            let $onPageElm = $(`div[id='${elm.name}']`);
+            adminUI.theme[key] = {};
+            adminUI.theme[key].options = [];
+            if ($onPageElm.length) {
+                let choices = $onPageElm.children('.elementAttributeChoice');
+                Array.from(choices).forEach(choice => {
+                    let name = $(choice).attr('id');
+                    let value = $(choice).find('option:selected').val();
+                    let subjectSpecific = elm.subject !== 'all';
+                    adminUI.theme[key].options.push({ name: name, value: value, subjectSpecific: subjectSpecific });
+                });
+            } else {
+                config.elements[key].options.forEach(opt => {
+                    let name = Object.keys(opt)[0];
+                    let value = opt[name][0];
+                    let subjectSpecific = elm.subject !== 'all';
+                    adminUI.theme[key].options.push({ name: name, value: value, subjectSpecific: subjectSpecific });
+                });
+            }
+        });
+
+        // Have theme object - export to .less variables file.
+        let varsFile = "";
+        varsFile += "@subjects: ";
+        config.subjects.forEach(subject =>
+            varsFile += `${subject}, `
+        );
+        varsFile = varsFile.slice(0, -2) + ";\n\n";
+
+        Object.keys(adminUI.theme).forEach(key => {
+            let tagName = config.elements[key].tagName;
+            adminUI.theme[key].options.forEach(option => {
+                config.gradeLevels.forEach(gradeLevel => {
+                    config.subjects.forEach(subject => {
+                        // option name, value
+                        varsFile += `@${gradeLevel}_${subject}_${tagName}_${option.name}: ${option.value};\n`;
+                    });
+                });
+            });
+        });
+        alert(varsFile);
+        //$('#examplePane').text(varsFile);
     }
 };
 
-// build up theme object as selections are made
-let theme = {};
-
-// let $elmChoices = $("#elmChoices"),
-//     $elmOpts = $("#elmOpts"),
-//     $attrOpts = $("#attrOpts");
-
 $(document).ready(function() {
 
-
-
-    // get grade level + subject to filter elm options
-
-    loadChoices();
-
-
-    loadListeners();
-    // then
+    adminUI.initUI();
 
 });
 
-function loadOptionsForElm(choice) {
-    let $elmOpts = $("#elmOpts");
-    let opts = config[choice];
-    let attrs = Object.keys(opts);
-    $elmOpts.empty();
-    attrs.forEach(function(attr) {
-        let option = $('<option/>');
-        option.text(attr);
-        option.val(attr);
-        $elmOpts.append(option);
-    });
-    $elmOpts.attr('size', attrs.length);
-    loadOptionsForAttr(opts[$elmOpts.find("option:first-child").val()]);
-}
 
-function loadOptionsForAttr(opts) {
-    let $attrOpts = $("#attrOpts");
-    $attrOpts.empty();
-    opts.forEach(function(opt) {
-        let option = $('<option/>');
-        option.text(opt);
-        option.val(opt);
-        $attrOpts.append(option);
-    });
-    $attrOpts.attr('size', opts.length);
-}
-
-function loadChoices() {
-    // If prev. selected, want that choice to appear as default
-    // todo
-    let elms = Object.keys(config);
-    let $elmChoices = $("#elmChoices");
-    elms.forEach(function(elm) {
-        let option = $('<option/>');
-        option.text(elm);
-        option.val(elm);
-        $elmChoices.append(option);
-    });
-    $elmChoices.attr('size', elms.length);
-
-    loadOptionsForElm($elmChoices.find("option:first-child").val());
-}
-
-function loadListeners() {
-    let $elmChoices = $("#elmChoices");
-    let $elmOpts = $("#elmOpts");
-    let $attrOpts = $("#attrOpts");
-    $elmChoices.change(function() {
-        loadOptionsForElm($elmChoices.find("option:selected").val());
-    });
-    $elmOpts.change(function() {
-        let choice = config[$elmChoices.find("option:selected").val()];
-        loadOptionsForAttr(choice[$elmOpts.find("option:selected").val()]);
-    });
-    $attrOpts.change(function() {
-        let choice = $elmChoices.find("option:selected").val();
-        let opt = $elmOpts.find("option:selected").val();
-        let attr = $attrOpts.find("option:selected").val();
-        if (!theme.hasOwnProperty(choice)) {
-            theme[choice] = {};
-        }
-        theme[choice][opt] = attr;
-    })
-
-}
-
-function exportTheme() {
-    console.log(theme);
-}
+// From StackOverflow.
+Object.filter = (obj, predicate) =>
+    Object.keys(obj)
+        .filter(key => predicate(obj[key]))
+        .reduce((res, key) => (res[key] = obj[key], res), {});
